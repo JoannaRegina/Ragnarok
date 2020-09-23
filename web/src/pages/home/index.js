22a31025d7436f2acdd979f2c8830193d86514a3
JoannaRegina <joannaregina1@gmail.com>
2020-09-23 13:53:31 -0300

    implemetação do filtro por plataforma

--- web/src/pages/home/index.js
+++ web/src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Component } from 'react'
 import api from '../../services/api'
 import './styles.css'
 
@@ -12,59 +12,120 @@ import IconWiiu from '../../assets/images/wiiu-platform.png'
 
 import { Link } from 'react-router-dom'
 
-export default function Home() {
+// export default function Home() {
+export default class Home extends Component {
 
-    const [games, setGames] = useState([])
-    const [platform, setPlatform] = useState('Todas')
-    const [error, setError] = useState(false)
-
-    useEffect(() => {
-        async function loadGames() {
-            const response = await api.get('/games').then(response => {
-                setGames(response.data)
-                console.log(response.data)
-            }).catch(error => {
-                if (!error.response) {
-                    setError(true)
-                    console.log('AAAAAAAAAAAAAAAAAAAAA')
-                } else {
-                    this.errorStatus = error.response.data.message;
-                }
-            })
-
-        }
-
-        loadGames()
-    }, [])
+    // const [games, setGames] = useState([])
+    // const [platform, setPlatform] = useState('Todas')
+    // const [error, setError] = useState(false)
 
     // useEffect(() => {
-    //     if (platform == 'pc') {
-    //         async function loadGames() {
-    //             // const response = await api.get(`/games/platform/${platform}`)
-    //             // setGames(response.data)
-    //             // console.log(response.data)
-    //             alert('CLICADO')
-    //         }
-
+    //     if (platform == 'Todas') {
     //         loadGames()
+    //     } else {
+    //         loadGamesFiltered()
     //     }
-    // }, [1000])
+    // }, [])
 
-    // const loadGamesFiltered = () => {
-    //     if (platform !== 'Todas') {
-    //         const response = api.get(`/games/platform/${platform}`)
+    // const loadGames = async () => {
+    //     await api.get('/games').then(response => {
     //         setGames(response.data)
     //         console.log(response.data)
-    //         alert('CLICADO')
-    //     }
+    //     }).catch(error => {
+    //         if (!error.response) {
+    //             setError(true)
+    //         } else {
+    //             this.errorStatus = error.response.data.message;
+    //         }
+    //     })
+
     // }
 
-    const addZeros = (num) => {
+    // const loadGamesFiltered = async (platform) => {
+    //     await api.get(`/games/platform/${platform}`).then(response => {
+    //         console.log(response.data)
+    //         setPlatform(platform)
+    //         setGames(response.data)
+
+    //     }).catch(
+
+    //     )
+
+    // }
+
+    constructor() {
+        super()
+        this.state = {
+            games: [],
+            platform: '',
+            error: false
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount = () => {
+        this.loadGames()
+    }
+
+    // carregando os jogos filtrados
+    componentDidUpdate = () => {
+        if (this.state.platform !== 'Todas') {
+            this.loadGamesFiltered()
+        }
+    }
+
+    // carregando jogos
+    loadGames = async () => {
+        this.setState({ platform: 'Todas' })
+        await api.get('/games').then(response => {
+            this.setState({ games: response.data })
+        }).catch(error => {
+            if (!error.response) {
+                this.setState({ error: true })
+            } else {
+                this.errorStatus = error.response.data.message;
+            }
+        })
+
+    }
+
+    // buscando os jogos baseado na plataforma selecionada
+    loadGamesFiltered = async (platform) => {
+        let plat = platform
+
+        await api.get(`/games/platform/${plat}`).then(response => {
+            this.setState({ platform: platform, games: response.data })
+        }).catch(error => {
+            console.log(error.response.data.message)
+        }
+
+        )
+
+    }
+
+    // capturando o valor do evento e carregando
+    // a lista de jogos
+    handleChange(event) {
+        let value = event.target.value
+        this.setState({ platform: value });
+
+        this.loadGamesFiltered(value)
+    }
+
+    // função responsável por verificar e adicionar um oi dois
+    // zeros ao final do número
+    // ao final o numero é retornado com a substituição do (.) pela (,)
+    addZeros = (num) => {
         let addZeros = num.toFixed(Math.max(((num + '').split(".")[1] || "").length, 2));
 
         return addZeros.toString().replace(".", ",")
     }
 
+
+render() {
+
+    const { games, platform, error } = this.state
 
     return (
         <div id="app">
@@ -88,10 +149,10 @@ export default function Home() {
                             <div id="platform-block">
                                 <img src={IconPs} alt="Ícone plataforma Playstation" />
 
-                                <select name="playstation" id="">
-                                    <option selected value="none">
+                                <select name="playstation" onChange={this.handleChange}>
+                                    <option disabled selected value="none">
                                         playstation
-                            </option>
+                                        </option>
 
                                     <option value="ps3">PS3</option>
                                     <option value="ps4">PS4</option>
@@ -101,28 +162,28 @@ export default function Home() {
                             <div id="platform-block">
                                 <img src={IconXbox} alt="Ícone plataforma XBox" />
 
-                                <select name="xbox" id="">
-                                    <option selected value="none">
+                                <select onChange={this.handleChange} name="xbox">
+                                    <option disabled selected value="none">
                                         XBOX
-                            </option>
+                                    </option>
 
                                     <option value="xbox">XBox</option>
                                     <option value="xboxOne">XBox One</option>
                                 </select>
                             </div>
 
-                            <div onClick={() => setPlatform('pc')} id="platform-block">
+                            <div onClick={() => this.loadGamesFiltered('pc')} id="platform-block">
                                 <img src={IconPc} alt="Ícone plataforma computador" />
                                 <a href="#">PC</a>
                             </div>
 
 
-                            <div id="platform-block">
+                            <div onClick={() => this.loadGamesFiltered('switch')} id="platform-block">
                                 <img src={IconSwitch} alt="Ícone plataforma Switch Nitendo" />
                                 <a href="">SWITCH</a>
                             </div>
 
-                            <div id="platform-block">
+                            <div onClick={() => this.loadGamesFiltered('wiiu')} id="platform-block">
                                 <img src={IconWiiu} alt="Ícone plataforma Wiiu Nitendo" />
                                 <a href="">WIIU</a>
                             </div>
@@ -142,21 +203,26 @@ export default function Home() {
                                     style={{ color: '#30c949', fontSize: 17 }}>
                                     {platform} </strong>
                                 </h4>
-                                {platform !== 'Todas' ?
-                                    <span onClick={() => setPlatform('Todas')}>Desfazer</span> : null
-                                }
-                            </div>
 
+                                {/* certificando de que a opção "desfazer" aparecerá apenas 
+                                    quando uma plataforma for selecionada 
+                                */}
+                                {platform !== 'Todas' ?
+                                    <span onClick={() => this.loadGames()}>Desfazer</span> : null
+                                }
+
+                            </div>
                         </header>
 
                         <main id="container-games">
-                                {
-                                    (error == false && games.length == 0) ?
-                                        <strong className="error">Carregando jogos...</strong> :
-                                        error == true && games.length == 0 ?
-                                            <strong className="error">nada encontrado. Verifique sua conexão</strong> : null
-                                }
-                            {/* </div> */}
+                        {/* será exibido uma mensagem de carregamento enquanto a requisição estiver sendo feita */}
+                        {/* uma mensagem de erro irá informar o usuário de que não foi possível fazer a reqisição */}
+                            {
+                                (error == false && games.length == 0) ?
+                                    <strong className="error">Carregando jogos...</strong> :
+                                    error == true && games.length == 0 ?
+                                        <strong className="error">nada encontrado. Verifique sua conexão</strong> : null
+                            }
 
                             {games.map(game => (
                                 <Link to={`/games/id/${game.id}`}>
@@ -166,7 +232,7 @@ export default function Home() {
                                             <h4>{game.nome}</h4>
                                             <h6>{game.plataformas}</h6>
                                             <h3>
-                                                R${addZeros(game.valor)}
+                                                R${this.addZeros(game.valor)}
                                             </h3>
                                         </div>
                                     </div>
@@ -178,4 +244,5 @@ export default function Home() {
             </div>
         </div >
     )
+}
 }
